52e4b5ec0dc63d7b2cd2eff222eabeba14ed7670
master-dhanesh <dhanesh1296@gmail.com>
2025-07-08 18:10:31 +0530

    SSR CSR

--- pages/404.js
+++
@@ -1,5 +0,0 @@
-const PageNotFound = () => {
-    return <div>404</div>;
-};
-
-export default PageNotFound;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,36 @@
-const Homepage = () => {
-    return <div>index</div>;
+import path from "path";
+import fs from "fs";
+import Link from "next/link";
+const Homepage = ({ products }) => {
+    return (
+        <div className="py-10 px-[10%] font-thin">
+            {products.map((product) => (
+                <div key={product.id}>
+                    <Link className="text-xl" href={`/product/${product.id}`}>
+                        {product.title}
+                    </Link>
+                </div>
+            ))}
+        </div>
+    );
 };
+
+export async function getStaticProps() {
+    console.log("Re-Generating...");
+    const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+    const jsonData = fs.readFileSync(filePath, "utf-8");
+    const data = JSON.parse(jsonData);
+    if (!data || !data.products || data.products.length === 0) {
+        return {
+            notFound: true,
+        };
+    }
+    return {
+        props: {
+            products: data.products,
+        },
+        revalidate: 5,
+    };
+}
 
 export default Homepage;
